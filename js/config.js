2a5065a333c7f3c77220f0fd96dbf70438a144ab
Alexander Green <alexander.green@logius.nl>
2023-06-15 14:12:19 +0200

    patch versienummer en -datum

--- js/config.js
+++ js/config.js
@@ -20,11 +20,11 @@ var respecConfig = {
       }
     ],
   github: "https://github.com/Logius-standaarden/Digikoppeling-Identificatie-en-Authenticatie",
-  previousPublishDate: "2017-10-01",
-  previousPublishVersion: "1.4.1",
+  previousPublishDate: "2022-04-11",
+  previousPublishVersion: "1.4.2",
   pubDomain: "dk",
-  publishDate: "2022-04-11",
-  publishVersion: "1.4.2",
+  publishDate: "2023-06-15",
+  publishVersion: "1.4.3",
   shortName: "idauth",
   specStatus: "DEF",
   specType: "ST"
